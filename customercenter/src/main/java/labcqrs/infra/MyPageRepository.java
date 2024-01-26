3b299aff10c22bdd082a3c6d1c774f23b1579d3d
KimKyusoo <123912988+kyusooK@users.noreply.github.com>
2024-01-26 10:49:21 +0900

    Update MyPageRepository.java

--- customercenter/src/main/java/labcqrs/infra/MyPageRepository.java
+++ customercenter/src/main/java/labcqrs/infra/MyPageRepository.java
@@ -1,6 +1,7 @@
 package labcqrs.infra;
 
 import java.util.List;
+import java.util.Optional;
 import labcqrs.domain.*;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
